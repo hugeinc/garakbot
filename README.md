01d2d63c1fb4c4b91c7cd1b19d753615532581c8
HUGE | Isaiah Belle <ibelle@ibelle-ny-05589.home>
2015-03-01 16:06:53 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+GARAK HipChat BOT
---
+++ fabfile.py
@@ -0,0 +1,16 @@
+from fabric.api import local, run, env
+import datetime, os
+#env.hosts = ['host1', 'host2']
+#/Users/ibelle/.virtualenvs/garak/bin/err.py
+
+now = datetime.datetime.today()
+timestamp = now.strftime("%Y%m%d%H%M")
+venv_path = os.environ['VIRTUAL_ENV']
+env.user = os.environ['USER']
+env.sudo_user = env.user
+
+def start_err(dameon=None):
+    if not dameon:
+        dameon=''
+
+    local("err.py -c data/  %s" % dameon)
